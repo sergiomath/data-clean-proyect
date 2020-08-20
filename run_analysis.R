5995e4c25b3161353490573944babe65899e0b9a
sergiomath <30483470+sergiomath@users.noreply.github.com>
2020-08-20 02:25:30 -0500

    final test

--- run_analysis.R
+++ run_analysis.R
@@ -1,23 +1,8 @@
 
 library(reshape2)
-#1. get dataset from web
-rawDataDir <- "./rawData"
-rawDataUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-rawDataFilename <- "rawData.zip"
-rawDataDFn <- paste(rawDataDir, "/", "rawData.zip", sep = "")
-dataDir <- "./data"
-
-if (!file.exists(rawDataDir)) {
-  dir.create(rawDataDir)
-  download.file(url = rawDataUrl, destfile = rawDataDFn)
-}
-if (!file.exists(dataDir)) {
-  dir.create(dataDir)
-  unzip(zipfile = rawDataDFn, exdir = dataDir)
-}
-
-
-#2. merge {train, test} data set
+## first step download from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+##and unzip in our workdirectory
+## merge train and  test data set
 
 x_train <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/train/X_train.txt"))
 y_train <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/train/Y_train.txt"))
@@ -28,21 +13,17 @@ x_test <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/test/X_test.txt"
 y_test <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/test/Y_test.txt"))
 s_test <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/test/subject_test.txt"))
 
-# merge {train, test} data
+# merge 
 x_data <- rbind(x_train, x_test)
 y_data <- rbind(y_train, y_test)
 s_data <- rbind(s_train, s_test)
 
 
-#3. load feature & activity info
-# feature info
+##load feature
 feature <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/features.txt"))
-
-# activity labels
 a_label <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/activity_labels.txt"))
 a_label[,2] <- as.character(a_label[,2])
 
-# extract feature cols & names named 'mean, std'
 selectedCols <- grep("-(mean|std).*", as.character(feature[,2]))
 selectedColNames <- feature[selectedCols, 2]
 selectedColNames <- gsub("-mean", "Mean", selectedColNames)
@@ -50,17 +31,16 @@ selectedColNames <- gsub("-std", "Std", selectedColNames)
 selectedColNames <- gsub("[-()]", "", selectedColNames)
 
 
-#4. extract data by cols & using descriptive name
+##in this step extract data by columns 
 x_data <- x_data[selectedCols]
 allData <- cbind(s_data, y_data, x_data)
 colnames(allData) <- c("Subject", "Activity", selectedColNames)
-
 allData$Activity <- factor(allData$Activity, levels = a_label[,1], labels = a_label[,2])
 allData$Subject <- as.factor(allData$Subject)
 
 
-#5. generate tidy data set
+###in this step we generate tidy data set
 meltedData <- melt(allData, id = c("Subject", "Activity"))
 tidyData <- dcast(meltedData, Subject + Activity ~ variable, mean)
-
+##finally  write the tidy dataset
 write.table(tidyData, "./tidy_dataset.txt", row.names = FALSE, quote = FALSE)
